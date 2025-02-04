0cb39f86f3d0099381636410c2267630cb53f7c7
Cristian Gutierrez <cdgutierrez456@gmail.com>
2025-02-03 20:19:14 -0500

    Enrutamiento

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ const Index = () => {
   return (
     <View>
       <Text className='text-4xl text-red-600'>Index</Text>
-      <Link href='/profile'>
+      <Link href='/account/878'>
         <Text className='rounded p-4 bg-slate-200 text-5xl'>
           Profile
         </Text>
