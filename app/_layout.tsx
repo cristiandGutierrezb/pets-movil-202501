771fc7870201bcd4bd753360f367c9c2c06c044b
Cristian Gutierrez <cdgutierrez456@gmail.com>
2025-04-19 00:10:23 -0500

    Cambios varios

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,23 @@
 import { useEffect, useState } from "react";
-import { Stack, Slot } from "expo-router";
+import { Stack } from "expo-router";
 import NetInfo from '@react-native-community/netinfo'
+
+import { usePushNotifications } from "../hooks/usePushNotifications";
 
 import "../global.css";
 
 const HomeLayout = () => {
 
+  const { expoPushToken, notification } = usePushNotifications()
+
   const [isConnected, setIsConnected] = useState(false)
 
   useEffect(() => {
+
+    console.log("expoPushToken", expoPushToken)
+    console.log("notification", notification);
+    
+
     const unsubscribe = NetInfo.addEventListener(state => {
       setIsConnected(state.isConnected as boolean)
     })
